4442319f7a0a893c6fad67e4302897233c205a8e
stefanos.amanuel <34087181+ipkstef@users.noreply.github.com>
2023-08-12 07:07:07 +0000

    working

--- auto-video/main.py
+++ auto-video/main.py
@@ -1,4 +1,3 @@
-from datetime import date
 import ffmpeg
 import csv
 import random
@@ -42,15 +41,28 @@ def get_random_mp4_file(folder_path):
         return None
     return os.path.join(folder_path, random.choice(mp4_files))
 
-def overlay_text_on_video(input_video_path, output_video_path, text):
+def get_random_mp3_file(folder_path):
+    mp3_files = [file for file in os.listdir(folder_path) if file.lower().endswith('.mp3')]
+    if not mp3_files:
+        return None
+    return os.path.join(folder_path, random.choice(mp3_files))
+
+def overlay_text_on_video(input_video_path, output_video_path,input_audio_path, text):
     (
         ffmpeg
         .input(input_video_path)
-        .filter('drawtext', text=text, fontsize=40, fontcolor='white', x='(w-text_w)/2', y='(h-text_h)/2', boxborderw=5, font='Aharoni-Bold')
-        # .output(output_video_path, vcodec='libx264', crf=20, preset='faster', movflags='faststart')
-        .output(output_video_path, vcodec='libx264', acodec='copy', crf=20, preset='faster', movflags='faststart')
+        .filter('drawtext', text=text, fontsize=60, fontcolor='white', x='(w-text_w)/2', y='(h-text_h)/2', borderw=3, bordercolor='black')
+        .output('temp_video.mp4', vcodec='libx264', crf=20, preset='faster', movflags='faststart', pix_fmt='yuv420p')
         .run()
     )
+    (
+        ffmpeg
+        .output(ffmpeg.input('temp_video.mp4'), ffmpeg.input(input_audio_path), output_video_path, shortest=None,vcodec='libx264', crf=20, preset='faster', movflags='faststart', pix_fmt='yuv420p')
+        .run()
+    )
+    os.remove('temp_video.mp4')
+    
+
 
 def wrapped_text(text, max_line_length=30):
     words = text.split()
@@ -66,20 +78,23 @@ def wrapped_text(text, max_line_length=30):
     return "\n\n".join(lines)
 
 if __name__ == "__main__":
+
     quote = generate_random_quote(csv_file, start_row)
     wrapped_quote = wrapped_text(quote)
     print(quote)
     video_folder_path = "auto-video/input"
     output_video_path = f"auto-video/output/output{video_id}.mp4"
-    print(f"Output video path: {output_video_path}")
     input_video_path = get_random_mp4_file(video_folder_path)
-    if input_video_path:
-        try:
-            overlay_text_on_video(input_video_path, output_video_path, text=wrapped_quote)
-            print("Overlay complete.")
-        except Exception as e:
-            print(f"Error: {e}")
-            
+    input_audio_path = get_random_mp3_file(video_folder_path)
+
+    try:
+        overlay_text_on_video(input_video_path, output_video_path,input_audio_path, text=wrapped_quote)
+        print("Overlay complete.")
+        exit(0)
+    except Exception as e:
+        print(e)
+        print("Error occurred while overlaying text on video.")
+        os.remove('temp_video.mp4')
     else:
         print("No MP4 files found in the folder.")
 
